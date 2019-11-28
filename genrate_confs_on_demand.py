71de4866c7e8ea7a2f45d99f0f023cbbcc5e1a4c
Abhik Mukhopadhyay <abhikuohyd@gmail.com>
2019-11-28 16:50:47 +0000

    modified

--- genrate_confs_on_demand.py
+++ genrate_confs_on_demand.py
@@ -2,6 +2,19 @@ import os
 import argparse
 from ccdc.io import MoleculeReader, MoleculeWriter
 from ccdc.conformer import ConformerGenerator, ConformerSettings
+import multiprocessing
+from ttictoc import TicToc
+
+def read_sdf_file(directory):
+    '''
+    read .picked file and create genrator
+    :param directory: directory where .picked files are
+    :return: generator
+    '''
+    if os.path.exists(directory):
+        for filename in os.listdir(directory):
+            if filename.endswith('.sdf'):
+                yield filename
 
 
 def standardise(mol):
@@ -29,23 +42,48 @@ def generate_confs(mol, nconformers, nthreads):
     confs = gen.generate(standardised_mol)
 
     return confs
-
-
-def main():
+def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sdf_mols', '-sdf', help='sdf file to generate conformers')
+    parser.add_argument('--sdf_dir', '-dir', help='sdf file to generate conformers')
     parser.add_argument('--conformers_file_dir', '-out', help='directory where to write out conformmers')
+    parser.add_argument('--number_of_conformers', '-n', help='number of conformers to generate')
     args = parser.parse_args()
+    return args
 
-    molecules = args.sdf_mols
-    list_of_molecules = [m for m in MoleculeReader(molecules)]
-    for mol in list_of_molecules:
+class Conformer_generator:
+    def __init__(self, args):
+        self.args = args
 
-        conformers = generate_confs(mol, 2, 1)
-        full_directory_path = os.path.join(args.conformers_file_dir, '%s_conformers.mol2' % mol.identifier)
+    def generate_conformer(self, mol):
+
+        conformers = generate_confs(mol, int(self.args.number_of_conformers), 8)
+        full_directory_path = os.path.join(self.args.conformers_file_dir, os.makedirs('{}'.format(mol.identifier)), '%s_conformers.mol2' % mol.identifier)
         with MoleculeWriter(full_directory_path) as mol_writer:
             for c in conformers:
                 mol_writer.write(c.molecule)
+
+
+
+def main():
+    #p = multiprocessing.Pool(8)
+    args = parse_arguments()
+    #molecules = args.sdf_mols
+    sdf_dir = os.path.join(args.sdf_dir)
+    list_of_sdf_files = [filename for filename in read_sdf_file(sdf_dir)]
+    list_of_molecules = []
+    proc = Conformer_generator(args)
+    t = TicToc()
+    t.tic()
+
+    for file in list_of_sdf_files:
+        for m in MoleculeReader(os.path.join(sdf_dir, file)):
+            list_of_molecules.append(m)
+
+    for mol in list_of_molecules:
+        proc.generate_conformer(mol)
+    #p.map(proc.generate_conformer, list_of_molecules)
+    t.toc()
+    print(t.elapsed)
 
 if __name__=='__main__':
      main()
