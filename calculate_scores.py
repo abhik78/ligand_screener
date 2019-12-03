fb5bfb9a119b227d36ad120d24235d77454b0780
Abhik Mukhopadhyay <abhikuohyd@gmail.com>
2019-12-03 11:02:46 +0000

    added ccdc scroing methods

--- calculate_scores.py
+++ calculate_scores.py
@@ -4,7 +4,9 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 import sklearn.metrics as metrics
-
+from ccdc.descriptors import StatisticalDescriptors
+import operator
+import collections
 def read_csv(filename):
     '''
     ead csv file
@@ -18,19 +20,29 @@ def read_csv(filename):
 
 
 def main():
-    rows = read_csv('screening_scores.csv')
+    rows = read_csv('/home/amukhopadhyay/ligand_screener_testing/screening_scores.csv')
     scores = []
     for row in rows:
         scores.append([row[0], int(row[1])])
 
-    #print(scores)
-    fractions = [0.01, 0.05, 0.1]
-    print(Scoring.CalcAUC(scores, 1))
-    print(Scoring.CalcBEDROC(scores, 1, 20))
-    print(Scoring.CalcEnrichment(scores, 1, fractions))
-    print(Scoring.CalcRIE(scores, 1, 20))
-    print((Scoring.CalcAUC(scores, 1)))
-    print((Scoring.CalcROC(scores, 1)))
+    #print(scores) rdkit methods
+    #fractions = [0.01, 0.05, 0.1]
+    #print(Scoring.CalcAUC(scores, 1))
+    #print(Scoring.CalcBEDROC(scores, 1, 20))
+    #print(Scoring.CalcEnrichment(scores, 1, fractions))
+    #print(Scoring.CalcRIE(scores, 1, 20))
+    #print((Scoring.CalcAUC(scores, 1)))
+    #print((Scoring.CalcROC(scores, 1)))
+
+    rank_stats = StatisticalDescriptors.RankStatistics(scores, activity_column=operator.itemgetter(1))
+    print(round(rank_stats.EF(0.01), 1))
+    print(round(rank_stats.EF(0.02), 1))
+    print(round(rank_stats.EF(0.05), 1))
+    print(round(rank_stats.EF(0.1), 1))
+    print(round(rank_stats.AUC(), 1))
+    print(round(rank_stats.BEDROC(alpha=20), 1))
+    print(round(rank_stats.RIE(alpha=20), 1))
+
 
     fpr, tpr = Scoring.CalcROC(scores, 1)
     roc_auc = metrics.auc(fpr, tpr)
